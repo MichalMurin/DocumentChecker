e5833d395ebeea07f0f2417e5eadaf2525a05dcd
Michal Murín <murin.michal.945@gmail.com>
2024-01-25 19:48:38 +0100

    Add project files.

---
+++ DocumentChecker/Pages/FormattingPage.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace DocumentChecker.Pages
+{
+    public class ReturnValue
+    {
+        public string Value { get; set; } = string.Empty;
+    }
+    public partial class FormattingPage
+    {
+        private int _currentCount = 0;
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; } = default!;
+        public IJSObjectReference JSModule { get; set; } = default!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                JSModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./Pages/FormattingPage.razor.js");
+            }
+        }
+
+        private async Task GetNumberOfWords()
+        {
+            ReturnValue ret = await JSModule.InvokeAsync<ReturnValue>("GetAllText");
+            var words = ret.Value.Split(" ");
+            _currentCount = words.Count();
+            //await JSModule.InvokeVoidAsync($"InsertText", $"Current count is {_currentCount}");
+        }
+    }
+}
---
+++ DocumentChecker/Pages/FormattingPage.razor.js
@@ -0,0 +1,42 @@
+﻿
+export async function InsertText(text) {
+    await Word.run(async (context) => {
+        
+
+        // Create a proxy object for the document body.
+        const body = context.document.body;
+        // Queue a command to insert text at the end of the document body.
+        body.insertText(text, Word.InsertLocation.end);
+        // Queue a command to get the current selection.
+        // Synchronize the document state by executing the queued commands,
+        // and return a promise to indicate task completion.
+        await context.sync();
+        console.log('Inserted text ');
+    })
+        .catch(function (error) {
+            console.log('Error: ' + JSON.stringify(error));
+            if (error instanceof OfficeExtension.Error) {
+                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+            }
+        });
+}
+
+export async function GetAllText() {
+    let text = "";
+    await Word.run(async (context) => {
+
+        // Create a proxy object for the document.
+        const body = context.document.body;
+        body.load("text");
+        await context.sync();
+        text = body.text;
+        console.log("Text is" + text);
+    })
+        .catch(function (error) {
+            console.log('Error: ' + JSON.stringify(error));
+            if (error instanceof OfficeExtension.Error) {
+                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+            }
+        });
+    return { Value: text };
+}
