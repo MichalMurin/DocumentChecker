bb1ed5af0b924614a44adab1a8d6c335c9228569
Michal Murín <murin.michal.945@gmail.com>
2024-02-03 22:01:07 +0100

    Implemented base structure of Gui components

---
+++ DocumentChecker/tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+    content: ['./**/*.{razor,html}',
+              "./Layout/**/*.razor"],
+  theme: {
+      extend: {
+          colors: {
+              pallette: {
+                  "primary-black": "#5b595a",
+                  "secondary-black": "#3e3838",
+                  "hover-black": "#151313"
+              },
+          },
+      },
+      screens: {
+          shopItemResize: "410px",
+          xxs: "350px",
+          xs: "475px",
+          s: "570px",
+          ggg: "540px",
+          xxmd: "500px",
+          xmd: "888px",
+          xlg: "980px",
+          headerResize: "650px",
+          larrge: "1200px",
+          larrrge: "1380px",
+          gallery: "1300px",
+          cards: "950px",
+          text: "790px",
+          contactResize: "750px",
+          footerResize: "1060px",
+          numbers: "1250px",
+          imageResize: "1550px",
+          textResize: "1150px",
+          ownerText: "800px",
+          pictures: "600px",
+          cardOpen: "728px",
+          productResize: "697px",
+          menuResize: "1000px",
+          hamburgerSize: "840px",
+          submitOrder: "904px",
+          shopItemLine: "1400px",
+          dotsRemove: "1420px",
+          paralaxResize: "1450px",
+          ...defaultTheme.screens,
+      },
+  },
+  plugins: [],
+}
+
